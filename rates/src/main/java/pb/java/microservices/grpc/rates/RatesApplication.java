e72c67506ce7043d853b9356abe772ba7ce5ee52
PKraciuk <73762395+PKraciuk@users.noreply.github.com>
2023-07-25 19:42:49 +0200

    Initial commit

---
+++ rates/src/main/java/pb/java/microservices/grpc/rates/RatesApplication.java
@@ -0,0 +1,13 @@
+package pb.java.microservices.grpc.rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatesApplication.class, args);
+	}
+
+}
